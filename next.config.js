7f360027669261dc34f0cbc4eb963d656d3bbd50
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-09-06 09:54:47 +0200

    add next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example-apis.vercel.app",
+        port: "",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
