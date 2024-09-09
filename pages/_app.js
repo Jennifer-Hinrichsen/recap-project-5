5e27e4e44c892b47cee27837fd715173b532808b
Melanie Adler <melanieadler@MelaniesMacBook.fritz.box>
2024-09-09 09:48:04 +0200

    Move data fetching logic to pages/_app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,35 @@
+import useSWR from "swr";
 import GlobalStyle from "../styles";
 
+const URL = "https://example-apis.vercel.app/api/art";
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
 export default function App({ Component, pageProps }) {
+  const { data, isLoading, error } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error || !data) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,7 @@
 import ArtPieces from "@/components/ArtPieces/ArtPieces";
 import SpotlightPiece from "@/components/SpotlightPiece/SpotlightPiece";
-import useSWR from "swr";
 
-const URL = "https://example-apis.vercel.app/api/art";
-
-async function fetcher(url) {
-  const response = await fetch(url);
-  if (!response.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-    error.info = await response.json();
-    error.status = response.status;
-    throw error;
-  }
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-
-export default function HomePage() {
-  const { data, isLoading, error } = useSWR(URL, fetcher);
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-  if (error || !data) {
-    return <h1>Error: {error}</h1>;
-  }
+export default function HomePage({ data }) {
   return (
     <div>
       <SpotlightPiece pieces={data} />
