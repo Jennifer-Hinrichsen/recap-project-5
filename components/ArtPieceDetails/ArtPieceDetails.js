769060388d7a351f4ee21dff166c95cf83e11915
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-09 18:40:24 +0200

    edit button and description style

--- components/ArtPieceDetails/ArtPieceDetails.js
+++ components/ArtPieceDetails/ArtPieceDetails.js
@@ -5,9 +5,9 @@ import styled from "styled-components";
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <>
-      <Link href="/art-pieces">
+      <StyledLink href="/art-pieces">
         <StyledButton>Go to Gallery</StyledButton>
-      </Link>
+      </StyledLink>
       <ArtPiecePreview
         image={image}
         title={title}
@@ -15,23 +15,42 @@ export default function ArtPieceDetails({ image, title, artist, year, genre }) {
         height={288}
         width={288}
       />
-      <div>
-        <p>Year: {year}</p>
-        <p>Genre: {genre}</p>
-      </div>
+      <StyledDescription>
+        <p>
+          <strong>Year: </strong>
+          {year}
+        </p>
+        <p>
+          <strong>Genre: </strong> {genre}
+        </p>
+      </StyledDescription>
     </>
   );
 }
 
 const StyledButton = styled.button`
-  background-color: #ffb069;
-  color: black;
+  background-color: #845e3c;
+  color: #c4c4c4;
   margin: 20px;
-  border-radius: 5px;
+  border-radius: 10px;
   cursor: pointer;
   font-size: 16px;
 
   &:hover {
-    background-color: #eeff00;
+    background-color: #47a51f;
   }
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+`;
+
+const StyledDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+  background-color: #d2d5db;
 `;
