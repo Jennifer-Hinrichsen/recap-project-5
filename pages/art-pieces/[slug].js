233e0163cc4fa461c7a69abb838e56ccab808459
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-09 10:26:22 +0200

    read query parameter slug from next/router and read query parameter slug from next/router

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,5 +1,20 @@
-import ArtPiecePreview from "@/components/ArtPiecePreview/ArtPiecePreview";
+import ArtPieceDetails from "@/components/ArtPieceDetails/ArtPieceDetails";
+import { useRouter } from "next/router";
 
-export default function ArtPieceDetails({ image, title, artist, year, genre }) {
-  return <ArtPiecePreview />;
+
+export default function ArtPiecePage() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const artPiece = /*artPiecesData*/.find((piece) => piece.slug === slug);
+
+  return (
+    <ArtPieceDetails
+      image={artPiece.imageSource}
+      title={artPiece.name}
+      artist={artPiece.artist}
+      year={artPiece.year}
+      genre={artPiece.genre}
+    />
+  );
 }
