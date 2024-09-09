bbe04337269936cf0c39c61fe37912fd1dfb7d9c
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-09 14:50:20 +0200

    add styled button and link to art-pieces page

--- components/ArtPieceDetails/ArtPieceDetails.js
+++ components/ArtPieceDetails/ArtPieceDetails.js
@@ -1,8 +1,13 @@
 import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
+import Link from "next/link";
+import styled from "styled-components";
 
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <>
+      <Link href="/art-pieces">
+        <StyledButton>Go to Gallery</StyledButton>
+      </Link>
       <ArtPiecePreview
         image={image}
         title={title}
@@ -17,3 +22,16 @@ export default function ArtPieceDetails({ image, title, artist, year, genre }) {
     </>
   );
 }
+
+const StyledButton = styled.button`
+  background-color: #ffb069;
+  color: black;
+  margin: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #eeff00;
+  }
+`;
