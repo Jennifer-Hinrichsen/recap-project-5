311522835ead6117a204bfd8cd8831704c8cdd5d
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-09 09:58:32 +0200

    Create the component ArtPieceDetails and Create the page pages/art-pieces/[slug]

---
+++ components/ArtPieceDetails/ArtPieceDetails.js
@@ -0,0 +1,5 @@
+import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
+
+export default function ArtPieceDetails({ image, title, artist, year, genre }) {
+  return <ArtPiecePreview />;
+}
